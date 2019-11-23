ebe922031c4e396d5c1e43ebc099d39e97c6e4a1
alkankaya <alkankaya@ALKANKAYAA07A>
2019-11-23 18:20:21 +0100

    aws lambda configurations

---
+++ OhpenCaseStudy/LambdaEntryPoint.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace OhpenCaseStudy.Api
+{
+    public class LambdaEntryPoint :
+        // When using an ELB's Application Load Balancer as the event source change 
+        // the base class to Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
+        Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        /// <summary>
+        /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
+        /// needs to be configured in this method using the UseStartup<>() method.
+        /// </summary>
+        /// <param name="builder"></param>
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseStartup<Startup>();
+        }
+    }
+}
--- OhpenCaseStudy/Program.cs
+++ OhpenCaseStudy/Program.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.RuntimeSupport;
+using Amazon.Lambda.Serialization.Json;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 
@@ -7,7 +13,20 @@ namespace OhpenCaseStudy.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            if (string.IsNullOrEmpty(Environment.GetEnvironmentVariable("AWS_LAMBDA_FUNCTION_NAME")))
+            {
+                CreateHostBuilder(args).Build().Run();
+            }
+            else
+            {
+                var lambdaEntry = new LambdaEntryPoint();
+                var functionHandler = (Func<APIGatewayProxyRequest, ILambdaContext, Task<APIGatewayProxyResponse>>)(lambdaEntry.FunctionHandlerAsync);
+                using (var handlerWrapper = HandlerWrapper.GetHandlerWrapper(functionHandler, new JsonSerializer()))
+                using (var bootstrap = new LambdaBootstrap(handlerWrapper))
+                {
+                    bootstrap.RunAsync().Wait();
+                }
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
