1291c9f669aeb74b9df9a5a1852a4740a2df5dd5
alkankaya <alkankaya@ALKANKAYAA07A>
2019-11-23 11:16:25 +0100

    added .net core api project

---
+++ OhpenCaseStudy/Controllers/StringSortController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OhpenCaseStudy.Controllers
+{
+    [ApiController]
+    [Route("StringSort")]
+    public class StringSortController : ControllerBase
+    {
+        private readonly ILogger<StringSortController> _logger;
+
+        public StringSortController(ILogger<StringSortController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
