95e90306dcaca2e22abdacb6c00fe4b9e12fc5af
alkankaya <alkankaya@ALKANKAYAA07A>
2019-11-23 11:59:55 +0100

    Test project added - Domain

---
+++ OhpenCaseStudy.Domain.Tests/StringSorterServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using OhpenCaseStudy.Domain.Services;
+using System;
+using FluentAssertions;
+
+namespace OhpenCaseStudy.Domain.Tests
+{
+    [TestFixture]
+    public class StringSorterServiceTests
+    {
+        private StringSortService _stringSorterService;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            _stringSorterService = new StringSortService();
+        }
+
+        [Test][Ignore("dummy test")]
+        public void When_StringServiceIsInitiated()
+        {
+            _stringSorterService.Should().NotBeNull();
+        }
+    }
+}
